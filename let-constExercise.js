37362b96bd025912bd0fb192c4ccb09459900802
nayani <yapancs@gmail.com>
2024-05-08 15:05:15 -0400

    update let-constExercise.js

---
+++ let-constExercise.js
@@ -0,0 +1,24 @@
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+// const PI = 3.14
+// PI = 42;   Uncaught TypeError: Assignment to constant variable.
+
+
+// Q1 - What is the difference between var and let?
+        // var can redeclare , reassign , mutate and it's function scoped. var can access a hoisted variable.
+        // let can't redeclare and it's block scoped.
+
+
+// Q2 - What is the difference between var and const?
+        // var can redeclare , reassign , mutate and it's function scoped. var can access a hoisted variable.
+        // const can't redeclare , reassign and it's block scoped.
+
+
+// Q3 - What is the difference between let and const?
+        // let can reassign.
+        // const can't reassign.
+
+
+// Q4 - What is hoisting?
+        // variable hoisting is when a variable declaration is moved to the top of their scope, prior to execution of the code.
